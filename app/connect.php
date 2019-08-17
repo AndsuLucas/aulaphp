2b1159851a54b1af9d2093d536e94098cabcfa26
Anderson Lucas <44175834+AndsuLucas@users.noreply.github.com>
2019-08-17 19:54:01 -0300

    Delete connect.php

--- app/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$connection = new PDO(
-    "mysql:host=localhost;charset=utf8;dbname=gerenciamentodefaltas",
-    "root","password"
-);
-$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
